7882a0d339e550de5d71ca73fe4a5f8b401acf74
olamide-ibiloye <ibiloyeolamide@gmail.com>
2024-08-12 23:10:13 +0100

    fix build failure

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {media} from 'sanity-plugin-media'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -11,7 +10,7 @@ export default defineConfig({
   projectId: 'exhx7ece',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool(), media()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
